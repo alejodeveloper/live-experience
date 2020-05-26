5d3e914667f4a9af61f9069918624e12ec78cbd8
Alejo <sespitia@platzi.com>
2020-05-25 20:35:45 -0500

    [add] channel component and react to package.json

---
+++ Javascript/components/channels/channel.js
@@ -0,0 +1,28 @@
+import React, { component } from 'react'
+
+class Channel extends component {
+    
+    onClickFunction(e) {
+        e.preventDefault();
+        const { setChannel, channel } = this.props;
+        setChannel(channel);
+    }
+
+    render() {
+        const { channel } = this.props;
+        return (
+            <li>
+                <a onClick={this.onClickFunction.bind(this)}>
+                    { channel.name }
+                </a>
+            </li>
+        )
+    }
+}
+
+Channel.propTypes = {
+    channel: React.PropTypes.object.isRequired,
+    setChannel: React.PropTypes.func.isRequired,
+}
+
+export default Channel
